fbc02105e4d2a06261977b199145f9f930fd16de
ulungprayitno <ulung.prayitno93@gmail.com>
2019-12-14 23:21:44 +0700

    update config

---
+++ build/precache-manifest.500d6ad12ed8bd06408e1babb6107f63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b69b13f8b0208876c62872ca4fceab7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "254fe7ecb961d67dbc8a",
+    "url": "/static/js/2.f135e6bc.chunk.js"
+  },
+  {
+    "revision": "7e70a161d695ec8da9cb5aef6f89a117",
+    "url": "/static/js/2.f135e6bc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5f9007942d8a58dc8ba",
+    "url": "/static/js/main.5f18c064.chunk.js"
+  },
+  {
+    "revision": "f91620d73a62795b5c98",
+    "url": "/static/js/runtime-main.424c8acd.js"
+  }
+]);
